8fbcf2da444e24b16a7e30b88d86f59209bf548f
Santhi Kumar Yadavalli <santhikumaryadavalli23@gmail.com>
2022-01-29 12:39:50 +0530

    completely change the UI

--- main.py
+++ main.py
@@ -1,46 +1,71 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+from config import abstract_api_key
 
 app = Flask(__name__, static_folder='css', template_folder='html')
 
+covid_api_endpoint = "https://disease.sh/v3/covid-19"
+ipgeolocation_api_endpoint = "https://ipgeolocation.abstractapi.com/v1"
 
-def get_data(query):
-    api_endpoint = "https://disease.sh/v3/covid-19"
-    response = requests.get(f"{api_endpoint}/{query}")
+
+def format_num(num):
+    return "{:,}".format(int(num))
+
+
+def get_covid_data(query):
+    response = requests.get(f"{covid_api_endpoint}/{query}")
     return response.json()
+
+
+def get_world_data():
+    data = get_covid_data("all")
+    data_dict = {
+        "cases": format_num(data["cases"]),
+        "active": format_num(data["active"]),
+        "recovered": format_num(data["recovered"]),
+        "deaths": format_num(data["deaths"]),
+    }
+    return data_dict
+
+
+def get_all_countries_data():
+    countries_data = get_covid_data("countries") # list of dicts of countries data
+    countries = sorted(countries_data, key=lambda k: k['cases'], reverse=True) # sort the list based on total cases
+    return countries
+
+
+def get_users_location_data():
+    # Getting User's Location
+    ip_address = request.remote_addr  # User's IP address
+    response = requests.get(ipgeolocation_api_endpoint, params={
+        'api_key': abstract_api_key,
+        #'ip_address': ip_address,
+        'fields': 'country,country_code,flag'
+    })
+    location = response.json()
+    
+    # Getting User's Country's covid data
+    covid_data = get_covid_data(f"countries/{location['country_code']}")
+    data_dict = {
+        "country": location['country'],
+        "flag_url": location['flag']['svg'],
+        "cases": format_num(covid_data['cases']),
+        "active": format_num(covid_data['active']),
+        "recovered": format_num(covid_data['recovered']),
+        "deaths": format_num(covid_data['deaths'])
+    }
+    return data_dict
     
 
 @app.route('/')
 def home():
-
-    world_data = get_data("all") # dict of world data
-    countries_data = get_data("countries") # list of dicts of countries data
-    
-    # Store Data in Variables
-    total_cases = '{:,}'.format(world_data["cases"])
-    active_cases = '{:,}'.format(world_data["active"])
-    deaths = '{:,}'.format(world_data["deaths"])
-    recovered = '{:,}'.format(world_data["recovered"])
-    
-    # Calculating death and recovery rate
-    d, r = world_data["deaths"], world_data["recovered"]
-    death_rate = f"{round((d / (d + r)) * 100, 2)}%"
-    recovery_rate = f"{round((r / (d + r)) * 100, 2)}%"
-
-    countries = sorted(countries_data, key=lambda k: k['cases'], reverse=True) # sort the list based on total cases
-    row_titles = ['#', 'Country', 'Total Cases', 'Deaths', 'Recovered', 'Active Cases', 'New Cases', 'New Deaths', 'Critical']
-    keys = ['cases', 'deaths', 'recovered', 'active', 'todayCases', 'todayDeaths', 'critical']
-
+    world_data = get_world_data()
+    countries_data = get_all_countries_data()
+    user_country_data = get_users_location_data()    
     return render_template('index.html',
-                           total_cases=total_cases,
-                           active_cases=active_cases,
-                           deaths=deaths,
-                           recovered=recovered,
-                           death_rate=death_rate,
-                           recovery_rate=recovery_rate,
-                           row_titles=row_titles,
-                           countries=countries,
-                           keys=keys,
+                           world_data=world_data,
+                           countries_data=countries_data,
+                           user_country_data=user_country_data
                            )
 
 
